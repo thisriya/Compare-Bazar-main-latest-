d55e8d2a9b7f4bfdd141a90964159308c74a5424
Riya Singh <singh.riyarajput2003@gmail.com>
2025-04-17 17:20:06 +0530

    commit

--- Compare-Bazar-main/src/components/Marketing.jsx
+++ Compare-Bazar-main/src/components/Marketing.jsx
@@ -460,7 +460,7 @@ const Marketing = () => {
     </section>
 
     {/* competitor */}
-    <div className="bg-white p-6 py-20 rounded-xl max-w-6xl mx-auto px-4">
+    <div className="p-6 py-20 rounded-xl max-w-6xl mx-auto px-4">
   <h3 className="text-4xl font-semibold text-gray-900 mb-4">How You Stack Against Competitors</h3>
   <div className="grid grid-cols-4 gap-2">
     {/* Header */}
@@ -514,7 +514,7 @@ const Marketing = () => {
 </div>
 
 {/* growth framework */}
-<div className="bg-gray-50 p-8 rounded-2xl py-20 max-w-6xl mx-auto">
+<div className=" p-8 rounded-2xl py-20 max-w-6xl mx-auto">
   <h3 className="text-4xl font-semibold mb-8">Our 90-Day Growth Framework</h3>
   <div className="relative">
     {/* Progress line */}
